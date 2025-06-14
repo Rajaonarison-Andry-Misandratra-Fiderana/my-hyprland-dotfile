d862bbe4543791ecf0cbf8b18a4f6757d9514715
ExiledMonarch <136102893+Rajaonarison-Andry-Misandratra-Fiderana@users.noreply.github.com>
2025-06-14 08:56:40 +0300

    Major changes
    New waybar design and cava implemented
    New wallpaper
    More smooth animation

---
+++ hypr/scripts/gamemode.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# Vérifie si les animations sont activées (donc si on est en mode normal)
+if [ "$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')" = 1 ]; then
+    # Active le mode performance et désactive les effets visuels
+    hyprctl --batch "\
+        dispatch toggleperformance;\
+        keyword animations:enabled 0;\
+        keyword decoration:shadow:enabled 0;\
+        keyword decoration:blur:enabled 0;\
+        keyword general:gaps_in 0;\
+        keyword general:gaps_out 0;\
+        keyword general:border_size 1;\
+        keyword decoration:rounding 0"
+    exit
+fi
+
+# Si déjà en mode jeu, on recharge (ou on pourrait faire un script inverse)
+hyprctl reload
