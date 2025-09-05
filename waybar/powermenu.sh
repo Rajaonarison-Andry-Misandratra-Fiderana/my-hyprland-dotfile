9c22ebc0fc1194ab53b017cd90f57c6f30e4d472
ExiledMonarch <136102893+Rajaonarison-Andry-Misandratra-Fiderana@users.noreply.github.com>
2025-09-06 00:55:25 +0300

    better animations, better waybar implementation

--- waybar/powermenu.sh
+++ waybar/powermenu.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
-BATTERY_PATH="/org/freedesktop/UPower/devices/battery_BAT0"
 
-INFO=$(upower -i $BATTERY_PATH | grep -E "state|time to empty|percentage")
+BATTERY_PATH="/org/freedesktop/UPower/devices/battery_BAT0"
+STATE_FILE="/tmp/battery_state_prev"
+
+# Lire infos batterie
+INFO=$(upower -i $BATTERY_PATH | grep -E "state|percentage")
 STATE=$(echo "$INFO" | grep state | awk '{print $2}')
 PERCENT=$(echo "$INFO" | grep percentage | awk '{print $2}' | tr -d '%')
-TIME=$(echo "$INFO" | grep -E "time to" | awk -F: '{print $2}' | xargs)
 
-# Déterminer icône + couleur selon le pourcentage
-if [ "$PERCENT" -le 20 ]; then
-    ICON=""; COLOR="#FF5555"
-elif [ "$PERCENT" -le 40 ]; then
-    ICON=""; COLOR="#FFAA00"
-elif [ "$PERCENT" -le 60 ]; then
-    ICON=""; COLOR="#FFFF55"
-elif [ "$PERCENT" -le 80 ]; then
-    ICON=""; COLOR="#55FF55"
-else
-    ICON=""; COLOR="#00FF00"
+# Lire l'état précédent pour détecter changement
+PREV_STATE=""
+if [ -f "$STATE_FILE" ]; then
+    PREV_STATE=$(cat $STATE_FILE)
 fi
 
-# Tooltip
-if [[ $STATE == "discharging" ]]; then
-    TOOLTIP="Remaining time: $TIME"
-elif [[ $STATE == "charging" ]]; then
-    TOOLTIP="Time to full charge: $TIME"
+# Enregistrer l'état actuel
+echo "$STATE" > $STATE_FILE
+
+# Déterminer icône + couleur
+if [[ $STATE == "charging" ]]; then
+    ICON=""
+    COLOR="#00FF00"
+    TOOLTIP="Charging"
 else
-    TOOLTIP="Battery state: $STATE"
+    if [ "$PERCENT" -le 20 ]; then
+        ICON=""; COLOR="#FF5555"
+    elif [ "$PERCENT" -le 40 ]; then
+        ICON=""; COLOR="#FFAA00"
+    elif [ "$PERCENT" -le 60 ]; then
+        ICON=""; COLOR="#FFFF55"
+    elif [ "$PERCENT" -le 80 ]; then
+        ICON=""; COLOR="#55FF55"
+    else
+        ICON=""; COLOR="#00FF00"
+    fi
+    TOOLTIP="Battery: $PERCENT%"
 fi
-# JSON pour Waybar avec icône et pourcentage de même couleur
+
+# JSON pour Waybar
 echo "{\"text\": \"<span color='$COLOR'>$ICON $PERCENT%</span>\", \"tooltip\": \"$TOOLTIP\"}"
