045983184f9c12672b2da0f7418ea0d0ea9e291a
ExiledMonarch <136102893+Rajaonarison-Andry-Misandratra-Fiderana@users.noreply.github.com>
2025-09-05 23:34:07 +0300

    Add files via upload

--- waybar/powermenu.sh
+++ waybar/powermenu.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
-mode=$(echo -e "Gaming\nNormal\nUltra Minimal" | wofi --dmenu --prompt "Mode d'alimentation")
+BATTERY_PATH="/org/freedesktop/UPower/devices/battery_BAT0"
 
-case "$mode" in
-    "Gaming") powerprofilesctl set performance ;;
-    "Normal") powerprofilesctl set balanced ;;
-    "Ultra Minimal") powerprofilesctl set power-saver ;;
-esac
+INFO=$(upower -i $BATTERY_PATH | grep -E "state|time to empty|percentage")
+STATE=$(echo "$INFO" | grep state | awk '{print $2}')
+PERCENT=$(echo "$INFO" | grep percentage | awk '{print $2}' | tr -d '%')
+TIME=$(echo "$INFO" | grep -E "time to" | awk -F: '{print $2}' | xargs)
 
-[ -n "$mode" ] && notify-send "⚡ Mode " "$mode"
+# Déterminer icône + couleur selon le pourcentage
+if [ "$PERCENT" -le 20 ]; then
+    ICON=""; COLOR="#FF5555"
+elif [ "$PERCENT" -le 40 ]; then
+    ICON=""; COLOR="#FFAA00"
+elif [ "$PERCENT" -le 60 ]; then
+    ICON=""; COLOR="#FFFF55"
+elif [ "$PERCENT" -le 80 ]; then
+    ICON=""; COLOR="#55FF55"
+else
+    ICON=""; COLOR="#00FF00"
+fi
+
+# Tooltip
+if [[ $STATE == "discharging" ]]; then
+    TOOLTIP="Remaining time: $TIME"
+elif [[ $STATE == "charging" ]]; then
+    TOOLTIP="Time to full charge: $TIME"
+else
+    TOOLTIP="Battery state: $STATE"
+fi
+
+# JSON pour Waybar avec icône et pourcentage de même couleur
+echo "{\"text\": \"<span color='$COLOR'>$ICON $PERCENT%</span>\", \"tooltip\": \"$TOOLTIP\"}"
