c5162ac22387f2e3a2f4266f94f5329a3a789a72
𝔈𝔵𝔦𝔩𝔢𝔡𝔐𝔬𝔫𝔞𝔯𝔠𝔥 <136102893+Rajaonarison-Andry-Misandratra-Fiderana@users.noreply.github.com>
2025-12-29 10:32:06 +0300

    rofi is back + autochange img

---
+++ hypr/scripts/update-rofi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CONFIG="$HOME/.config/waypaper/config.ini"
+TMP_IMG="/tmp/rofi_wallpaper.png"
+
+inotifywait -m -e close_write,move,create "$CONFIG" | while read -r; do
+
+  WALLPAPER=$(awk -F= '/^wallpaper[[:space:]]*=/{gsub(/^[ \t]+|[ \t]+$/, "", $2); print $2}' "$CONFIG")
+  WALLPAPER="${WALLPAPER/#\~/$HOME}"
+
+  [ ! -f "$WALLPAPER" ] && exit 0
+
+  magick "$WALLPAPER" \
+    -filter Lanczos \
+    -resize 500x \
+    "$TMP_IMG"
+
+done
